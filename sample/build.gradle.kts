1b6f706319f4d9cc9df5a99d8f65947c7c919f1e
thekalpeshpawar <pawarkalpesh@proton.me>
2026-01-12 14:13:57 +0530

    refactor: remove redundant comment in LoginScreen.kt
    Removes an outdated comment regarding dialog visibility logic for error and loading states in `LoginScreen`.

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -35,11 +35,6 @@ kotlin {
         }
     }
 
-    // Source set declarations.
-    // Declaring a target automatically creates a source set with the same name. By default, the
-    // Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
-    // common to share sources between related targets.
-    // See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
     sourceSets {
         commonMain.dependencies {
             api(projects.mifosAuthKmp)
