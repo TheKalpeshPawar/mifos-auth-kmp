a210d321235ea01cee7936069095601105f22fb4
thekalpeshpawar <pawarkalpesh@proton.me>
2026-01-12 14:11:58 +0530

    build: cleanup unused dependencies and comments in `mifos-auth-kmp-ui`
    Removes various commented-out dependencies (Koin, Ktor, Lifecycle, and Navigation) and empty source set configurations from the `mifos-auth-kmp-ui` build script to clean up the configuration file.

--- mifos-auth-kmp-ui/build.gradle.kts
+++ mifos-auth-kmp-ui/build.gradle.kts
@@ -48,42 +48,14 @@ kotlin {
             api(projects.mifosAuthKmp)
             api(projects.core.common)
 
-//            implementation(libs.jb.lifecycleViewmodel)
-//            implementation(libs.jb.lifecycleViewmodelSavedState)
-//            implementation(libs.jb.composeNavigation)
-//            implementation(libs.koin.core)
-//            implementation(libs.koin.compose)
-//            implementation(libs.koin.compose.viewmodel)
-//            api(libs.koin.annotations)
-
-
             implementation(libs.kotlinx.serialization.json)
-//            implementation(libs.ktor.client.core)
-//            implementation(libs.ktor.client.json)
-//            implementation(libs.ktor.client.logging)
-//            implementation(libs.ktor.client.serialization)
-//            implementation(libs.ktor.client.content.negotiation)
-//            implementation(libs.ktor.client.auth)
-//            implementation(libs.ktor.serialization.json)
 
-
-            // Compose dependencies
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.uiToolingPreview)
             implementation(compose.materialIconsExtended)
-        }
-        androidMain.dependencies {
-//            implementation(libs.ktor.client.okhttp)
-//            implementation(libs.koin.android)
-        }
-        nativeMain.dependencies {
-//            implementation(libs.ktor.client.darwin)
-        }
-        jvmMain.dependencies {
-//            implementation(libs.ktor.client.okhttp)
         }
     }
 
